98ec1dd3bf3b189d68bdaad90c117579c31102bf
amihan <amirkhan.shamgonov.0101@mail.ru>
2022-08-22 15:03:19 +0600

    redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -7,10 +7,29 @@ export const addMessageActionCreator = () => ({
 
 export const changeMessageActionCreator = (newMessage) => ({
     type: CHANGE_MESSAGE,
-    newMessage:newMessage,
+    newMessage: newMessage,
 });
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ],
+    newMessage: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_MESSAGE:
             let body = {
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -10,7 +10,18 @@ export const updateNewPostTextActionCreator = (text) => ({
     newText: text,
 });
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11},
+        {id: 3, message: 'Blabla', likesCount: 11},
+        {id: 4, message: 'Dada', likesCount: 11}
+    ],
+    newPostText: 'amirhan',
+}
+
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
